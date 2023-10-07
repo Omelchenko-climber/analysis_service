a37e9fd6b97be633d6e444b0f6bb6a69c895275d
Anton <omelchenko230783@gmail.com>
2023-10-07 21:55:35 +0200

    creating distribution package

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="analysis_service",
-    version="1.0.0",
+    version="2.0.0",
     description="This service can analyse folders with logs.",
     url='https://github.com/Omelchenko-climber/analysis_service',
     author="Anton Omelchenko",
